45681ca7c6de6baa5f50285ecb6d8532ebc67c17
Victor Pereira <Victorpereiradeveloper@gmail.com>
2023-04-30 15:35:12 -0300

    mod8
    mod8

---
+++ mod8/ExercicioModulo8/src/br/com/vpereira/NotaMedia.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package br.com.vpereira;
+
+/**
+ * @author Victor Pereira
+ *
+ */
+public class NotaMedia {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double nota1 = 9;
+		double nota2 = 5.5;
+		double nota3 = 4;
+		double nota4 = 8.5;
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		System.out.print("Media das notas: " + media);
+	}
+	
+}
