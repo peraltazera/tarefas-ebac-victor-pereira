b0b12093f3efae9b0c596cdf3922a08cca615cbe
Victor Pereira <Victorpereiradeveloper@gmail.com>
2023-04-29 22:21:11 -0300

    main

---
+++ mod5/ExercicioModulo5/src/br/com/vpereira/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.vpereira;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello " + args[0]);
+	}
+
+}
