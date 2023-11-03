051c5a53803ed08cc168d62d41e2ce62fc2ae417
Victor Pereira <victorgamedeveloper@gmail.com>
2023-11-03 11:08:54 -0300

    mod40
    mod40

---
+++ mod40/src/main/java/com/vpereira/mod40/Mod40Application.java
@@ -0,0 +1,14 @@
+package com.vpereira.mod40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod40Application {
+
+	public static void main(String[] args) {
+		System.out.println("Executando projeto Spring Boot");
+		SpringApplication.run(Mod40Application.class, args);
+	}
+
+}
---
+++ mod40/src/test/java/com/vpereira/mod40/Mod40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpereira.mod40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
